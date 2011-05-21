37253715465581af37529e94833b6c7f9f074f62
Jean-Nicolas Boulay Desjardins <jnbdzjnbdz@gmail.com>
2011-05-21 01:23:03 -0400

    added function name

---
+++ classes/controller/app_store.php
@@ -0,0 +1,25 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * App_Store controller class.
+ *
+ * @author Jean-Nicolas Boulay Desjardins (http://jean-nicolas.name/)
+ * @package default
+ * @version 0.1
+ */
+
+class Controller_App_Store extends Controller_App {
+
+	public function index() {
+
+		
+
+	}
+
+	public function register() {
+
+		
+
+	}
+
+} // End App_Store
